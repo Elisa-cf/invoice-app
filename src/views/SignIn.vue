f2fb4b2f839fdd93082ede61415e1d6e6b51c821
Elisa <elisacanyelles@gmail.com>
2023-08-27 11:17:23 +0200

    style sign in form

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -2,15 +2,49 @@
   <div
     class="bg-[#E9E9E9] h-screen w-screen flex flex-col justify-center items-center"
   >
-    <h1>Sign in in your Account</h1>
-    <div class="bg-white h-1/2 w-12">
-      <p><input type="text" placeholder="Email" v-model="email" /></p>
-      <p><input type="password" placeholder="Password" v-model="password" /></p>
-      <p v-if="errMsg">{{ errMsg }}</p>
-      <button @click="register">Submit</button>
-      <button @click="singInWithGoogle">Sign in With Google</button>
+    <div
+      class="bg-white flex flex-col justify-center items-center w-11/12 py-10 gap-4 rounded-md"
+    >
+      <h1 class="font-bold text-xl text-[#969696]">User Log in</h1>
+      <div
+        class="flex items-center justify-center gap-2 border border-[#E9E9E9] p-2 w-10/12 rounded-md"
+      >
+        <div class="w-4"><img :src="GoogleLogo" alt="" /></div>
+        <button @click="singInWithGoogle">Sign in With Google</button>
+      </div>
+      <div class="flex items-center space-x-2">
+        <div class="w-20 border-b border-gray-400"></div>
+        <span class="text-gray-600">or</span>
+        <div class="w-20 border-b border-gray-400"></div>
+            
+      </div>
+      <input
+        class="bg-[#F2F2F2] w-10/12 p-2 rounded-md focus:outline-none focus:ring-[#969696] focus:ring-2 caret-[#969696]"
+        type="text"
+        placeholder="Email"
+        v-model="email"
+      />
+      <input
+        class="bg-[#F2F2F2] w-10/12 p-2 rounded-md focus:outline-none focus:ring-[#969696] focus:ring-2 caret-[#969696]"
+        type="password"
+        placeholder="Password"
+        v-model="password"
+      />
+      <p
+        class="bg-[#FBDADA] border border-[#e30904] text-[#eb2300] w-10/12 p-1.5"
+        v-if="errMsg"
+      >
+        {{ errMsg }}
+      </p>
+      <div class="bg-[#3A7FC2] w-10/12 p-2 rounded text-center text-white">
+        <button @click="register">LOGIN</button>
+      </div>
+
       <p>
-        <span>No account?</span> <RouterLink to="/register">Sign up</RouterLink>
+        <span class="text-[#969696] font-bold">No account? </span>
+        <RouterLink to="/register" class="text-[#3A7FC2] font-bold"
+          >Sign up</RouterLink
+        >
       </p>
     </div>
   </div>
@@ -25,6 +59,7 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 import { useRouter } from 'vue-router';
+import GoogleLogo from '../assets/images/google-logo.png';
 
 const email = ref('');
 const password = ref('');
@@ -41,7 +76,7 @@ const register = () => {
       console.log(error.code);
       switch (error.code) {
         case 'auth/invalid-email':
-          errMsg.value = 'Invalid email';
+          errMsg.value = 'This email address is invalid';
           break;
         case 'auth/user-not-found':
           errMsg.value = 'No account with that email was found';
